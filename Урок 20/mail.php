f55dd3801c6c321f6391a563af9efeae0e5012e8
ilchenko <eternalsur@yandex.ru>
2017-03-01 00:27:36 +0300

    Lesson 20new

--- Урок 20/mail.php
+++ Урок 20/mail.php
@@ -1,42 +1,23 @@
-<?php 
-$from = $_POST['from'];
-$username = $_POST['username'];
-$email = $_POST['email'];
-$user_message = $_POST['user_message'];
-
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'eternalsur@gmail.com';                 // SMTP username
-$mail->Password = 'dFKlg26wI4yS954l';                           // SMTP password
-$mail->SMTPSecure = 'ssl';                          // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
-
-$mail->setFrom('tunerauto@inbox.ru', 'Ildar S. Zhiganshin');
-$mail->addAddress('111970@mail.ru', 'Joe User');     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Это сабджект сообщения';
-$mail->Body    = 'Это тело сообщения дальше жирное <b>блаблабла</b>';
-$mail->AltBody = 'Это сообщение в формате plain text';
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
-
+<?php
+//Принимаем постовые данные
+$from=$_POST['from'];
+$username=$_POST['username'];
+$email=$_POST['email'];
+$user_message=$_POST['user_message'];
+//Тут указываем на какой ящик посылать письмо
+$to = "eternalsur@gmail.com";
+//Далее идет тема и само сообщение
+// Тема письма
+$subject = "Заявка с сайта";
+// Сообщение письма
+$message = "
+С какой формы пришла заявка: ".htmlspecialchars($from). "<br>
+Имя пользователя: ".htmlspecialchars($username)."<br />
+Телефон: <a href='tel:$email'>".htmlspecialchars($email)."</a> <br>
+Сообщение: ".htmlspecialchars($user_message);
+// Отправляем письмо при помощи функции mail();
+$headers = "From: stastroi.ru <mail@stastroi.ru>\r\nContent-type: text/html; charset=UTF-8 \r\n";
+mail ($to, $subject, $message, $headers);
+// Перенаправляем человека на страницу благодарности и завершаем скрипт
+exit();
 ?>
